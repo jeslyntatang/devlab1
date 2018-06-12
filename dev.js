5de1b0bacc72d44bf3b8274db49a0df721ba7dac
Jeslyn Tatang <jeslyn.tatang@gmail.com>
2018-06-12 20:05:05 +0000

    portfolio

---
+++ dev.js
@@ -0,0 +1,8 @@
+//forever faithful Color Palette
+$('#about').click(function(){
+    $('.info').text("Hello, I'm Jeslyn and I am a twelfth grade student at Balboa High School with an interest in coding and creative writing.");
+});
+
+$('#links').click(function(){
+    $('.info').html("<a href='mailto:jeslyn.tatang@gmail.com'>E-mail</a> <a href='https://www.linkedin.com/in/jeslyn-tatang-30aa2b149/'>LinkedIn</a> <a href='https://github.com/jeslyntatang'>GitHub</a>");
+});
